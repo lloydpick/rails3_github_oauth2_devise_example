efc4cfc1eaf47299507d95f5f25ea0d031d3c847
Lloyd Pick <lloydpick@gmail.com>
2010-07-06 13:51:59 +0100

    give the app a better name

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Hawk::Application.initialize!
+GithubOauth::Application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Hawk::Application.routes.draw do |map|
+GithubOauth::Application.routes.draw do |map|
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
